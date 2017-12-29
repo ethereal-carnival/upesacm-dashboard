c4d44b82e630086da014e5e70c447249877f195e
crimson-carnival <vyommaitreya@gmail.com>
2017-12-29 13:25:16 +0530

    Added font-awesome and made everything responsive

--- assets/parts/header.php
+++ assets/parts/header.php
@@ -1,7 +1,11 @@
 <header>
     <div class="cover">
         <a href="/" class="brand" id="brand-title">Dashboard</a>
-        <a href="http://www.upesacmwomen.org" target="_blank"><img class="logo" src="/assets/images/ACM-W.png" /></a>
-        <a href="http://www.upesacm.org" target="_blank"><img class="logo" id="one" src="/assets/images/UPES ACM LOGO.png" /></a>
+        <a href="/" class="brand" id="brand-title-small"><i class="fa fa-angle-double-left"></i></a>
+
+        <div class="logo-wrapper">
+            <a href="http://www.upesacm.org" target="_blank"><img class="logo" id="one" src="/assets/images/UPES ACM LOGO.png" /></a>
+            <a href="http://www.upesacmwomen.org" target="_blank"><img class="logo" src="/assets/images/ACM-W.png" /></a>
+        </div>
     </div>
 </header>
--- assets/parts/includes.php
+++ assets/parts/includes.php
@@ -1,2 +1,3 @@
 <link href="/assets/css/common.css" rel="stylesheet" />
 <link href="/assets/css/index.css" rel="stylesheet" />
+<link href="/assets/css/font-awesome-4.7.0/css/font-awesome.css" rel="stylesheet" />
