7d3c7e4c321fb20d9e3e8d7acae27fa04fef965d
crimson-carnival <vyommaitreya@gmail.com>
2018-01-25 16:53:27 +0530

    News feed completed - front end

--- home/index.php
+++ home/index.php
@@ -10,42 +10,88 @@
     <head>
         <?php include '../assets/parts/includes.php'; ?>
         <link href="/assets/css/home.css" rel="stylesheet" />
+
+        <script type="text/javascript" src="../assets/js/home.js"></script>
     </head>
 
     <body>
         <?php include "../assets/parts/header-home.php"; ?>
         <div id="panel_wrapper">
             <section class="panel" id="panel_1">
-            </section>
-            <section class="panel" id="panel_2">
-                <article class="post">
-                    <div class="post_pic"></div>
-                    <div class="post_name">Vyom Maitreya</div>
-                    <div class="post_username">@crimson-carnival</div>
-                    <div class="post_caption">Don't leave me alone fellas!</div>
-                    <div class="post_event"><img src="/assets/images/event_2.jpg" style="height: 40vh;" /></div>
+                <article id="photograph">
+                    <button id="dp" onmouseover="document.getElementById('change_dp').style.opacity='0.9';" onmouseout="document.getElementById('change_dp').style.opacity='0';"><div id="change_dp"><span style="display: block; font-size: 4vh; margin-bottom: 0.5vh;"><i class="fa fa-camera"></i></span>Click to update display picture</div></button>
+                    <div class="post_name name">Vyom Maitreya</div>
+                    <center><a href="/people" style="color: #2870b8; text-decoration: none;"><i class="fa fa-search"></i>&nbsp;Find friends on Dashboard</a></center>
+                    <center><div style="margin-top: 2vh;"><a href="/logout" style="color: #dc143c; text-decoration: none;"><i class="fa fa-power-off"></i>&nbsp;Logout</a></div></center>
                 </article>
-                <br />
-                <article class="post">
-                    <div class="post_pic"></div>
-                    <div class="post_name">Vyom Maitreya</div>
-                    <div class="post_username">@crimson-carnival</div>
-                    <div class="post_caption">So excited to participate!</div>
-                    <div class="post_event"><img src="/assets/images/event_3.jpg" style="height: 40vh;" /></div>
-                </article>
-                <br />
-                <article class="post">
-                    <div class="post_pic"></div>
-                    <div class="post_name">Vyom Maitreya</div>
-                    <div class="post_username">@crimson-carnival</div>
-                    <div class="post_caption">So excited to participate!</div>
-                    <div class="post_event"><img src="/assets/images/event_4.jpg" style="height: 40vh;" /></div>
+
+                <article id="all-events">
+                    <div class="heading" style="width: 15vw;">All Events Participated In</div>
+                    <div class="upcoming"></div>
+                    <img class="small-event" src="/assets/images/event_1.jpg" />
+                    <img class="small-event" src="/assets/images/event_2.jpg" />
+                    <img class="small-event" src="/assets/images/event_3.jpg" />
+                    <img class="small-event" src="/assets/images/event_4.jpg" />
+                    <img class="small-event" src="/assets/images/event_5.jpg" />
                 </article>
             </section>
+
+            <?php include "panel_2.php"; ?>
+
             <section class="panel" id="panel_3">
                 <article id="upcoming-events">
+                    <div class="heading">My Upcoming Events</div>
+                    <ul class="upcoming">
+                        <li>International Coding League: 1st February</li>
+                        <li>Glitch: 1st February</li>
+                        <li>Frame of Reference: 1st February</li>
+                        <li>Engage: 1st February</li>
+                        <li>Echo: 1st February</li>
+                    </ul>
                 </article>
                 <article id="suggestions">
+                    <div class="heading">Discover More</div>
+                    <div style="margin-top: 4vh;"></div>
+                    <div class="packet">
+                        <img src="/assets/images/event_6.jpg" />
+                        <div class="discover_text">
+                            <div class="discover_name">Capture The Flag</div>
+                            <div class="discover_date">2nd February</div>
+                            <div class="discover_time">3pm - 5pm</div>
+                        </div>
+                    </div>
+                    <div class="packet">
+                        <img src="/assets/images/event_7.jpg" />
+                        <div class="discover_text">
+                            <div class="discover_name">Braille Code</div>
+                            <div class="discover_date">2nd February</div>
+                            <div class="discover_time">To Be Announced</div>
+                        </div>
+                    </div>
+                    <div class="packet">
+                        <img src="/assets/images/event_8.jpg" />
+                        <div class="discover_text">
+                            <div class="discover_name">Tuning Fork</div>
+                            <div class="discover_date">2nd February</div>
+                            <div class="discover_time">To Be Announced</div>
+                        </div>
+                    </div>
+                    <div class="packet">
+                        <img src="/assets/images/event_9.jpg" />
+                        <div class="discover_text">
+                            <div class="discover_name">Travelling Salesman</div>
+                            <div class="discover_date">2nd February</div>
+                            <div class="discover_time">To Be Announced</div>
+                        </div>
+                    </div>
+                    <div class="packet">
+                        <img src="/assets/images/event_10.jpg" />
+                        <div class="discover_text">
+                            <div class="discover_name">IBM Workshop</div>
+                            <div class="discover_date">2nd February</div>
+                            <div class="discover_time">To Be Announced</div>
+                        </div>
+                    </div>
                 </article>
             </section>
         </div>
---
+++ home/panel_2.php
@@ -0,0 +1,64 @@
+<section class="panel" id="panel_2">
+    <article class="post">
+        <div class="post_pic" style="display: inline; float: left; margin-top: 1vh;"><img src="/assets/images/dp.jpg" style="height: 4vh; display: inline; border-radius: 100px;" /></div>
+        <button class="button follow-status follow-status-checked">Following</button>
+        <div class="post_name">Vyom Maitreya</div>
+        <div class="post_username">@crimson-carnival</div>
+        <div class="post_caption">So excited to participate!</div>
+        <div class="post_event">
+            <div class="poster">
+                <img src="/assets/images/event_1.jpg" style="height: 40vh;" />
+            </div>
+            <div class="desc">
+                <div class="event_name event_detail">International Coding League</div>
+                <div class="event_date event_detail">Date: <strong>1st February, 2018</strong></div>
+                <div class="event_venue event_detail">Venue: <strong>10006</strong></div>
+                <div class="event_venue event_detail">Timings: <strong>1000-1600 hrs</strong></div>
+                <div class="event_participants event_detail">Particpants: <strong>48</strong></div>
+                <button class="button event_status event_detail" onclick="check(this)"><i class="fa fa-circle-o"></i>&nbsp;Participate</button>
+            </div>
+        </div>
+    </article>
+    <br />
+    <article class="post">
+        <div class="post_pic" style="display: inline; float: left; margin-top: 1vh;"><img src="/assets/images/dp2.jpg" style="height: 4vh; width: 4vh; display: inline; border-radius: 100px;" /></div>
+        <button class="button follow-status follow-status-checked">Following</button>
+        <div class="post_name">John Smith</div>
+        <div class="post_username">@the-doctor</div>
+        <div class="post_caption">So excited to participate!</div>
+        <div class="post_event">
+            <div class="poster">
+                <img src="/assets/images/event_9.jpg" style="height: 40vh;" />
+            </div>
+            <div class="desc">
+                <div class="event_name event_detail">Travelling Salesman</div>
+                <div class="event_date event_detail">Date: <strong>1st February, 2018</strong></div>
+                <div class="event_venue event_detail">Venue: <strong>10006</strong></div>
+                <div class="event_venue event_detail">Timings: <strong>1000-1600 hrs</strong></div>
+                <div class="event_participants event_detail">Particpants: <strong>48</strong></div>
+                <button class="button event_status event_detail" onclick="check(this)"><i class="fa fa-circle-o"></i>&nbsp;Participate</button>
+            </div>
+        </div>
+    </article>
+    <br />
+    <article class="post">
+        <div class="post_pic" style="display: inline; float: left; margin-top: 1vh;"><img src="/assets/images/dp.jpg" style="height: 4vh; display: inline; border-radius: 100px;" /></div>
+        <button class="button follow-status follow-status-checked">Following</button>
+        <div class="post_name">Vyom Maitreya</div>
+        <div class="post_username">@crimson-carnival</div>
+        <div class="post_caption">So excited to participate!</div>
+        <div class="post_event">
+            <div class="poster">
+                <img src="/assets/images/event_4.jpg" style="height: 40vh;" />
+            </div>
+            <div class="desc">
+                <div class="event_name event_detail">ENGAGE - Prodigy &apos;18</div>
+                <div class="event_date event_detail">Date: <strong>1st February, 2018</strong></div>
+                <div class="event_venue event_detail">Venue: <strong>10006</strong></div>
+                <div class="event_venue event_detail">Timings: <strong>1000-1600 hrs</strong></div>
+                <div class="event_participants event_detail">Particpants: <strong>48</strong></div>
+                <button class="button event_status event_detail" onclick="check(this)"><i class="fa fa-circle-o"></i>&nbsp;Participate</button>
+            </div>
+        </div>
+    </article>
+</section>
